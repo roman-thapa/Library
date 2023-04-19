aa4162117a2e8f937722b13b225866c58b0f07c1
roman <51534566+roman-thapa@users.noreply.github.com>
2023-04-19 17:02:37 +0530

    Add frature to add book and display it and design the html

--- script.js
+++ script.js
@@ -1,63 +1,119 @@
 let myLibary = [];
 let bookContainer = document.querySelector(".bookContainer");
 let newBookButton = document.querySelector(".newBookButton");
+let addBook = document.querySelector(".addBook");
+let title = document.querySelector("#title");
+let author = document.querySelector("#author");
+let pages = document.querySelector("#pages");
+let status = document.querySelector("#status");
+let visibilityOfForm = document.querySelector("#myDropdown")
 
-function Book(title, author, pages, status) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
+newBookButton.addEventListener("click", () => {
+  showForm();
+  clearForm();
+});
+
+function showForm() {
+  visibilityOfForm.classList.toggle("show");
 }
 
-Book.prototype.info = function() {
-    return(this.title + " by " + this.author
-        + ", "+ this.pages + " pages, " + this.status);
+function clearForm() {
+  title.innerHTML = "";
+  author.innerHTML = "";
+  pages.innerHTML = "";
 }
 
-Book.prototype.updateMyLibary = function() {
-        myLibary.push([{title: this.title, author: this.author, pages: this.pages, status: this.status}]);
+addBook.addEventListener('click', (event) => {
+  let flag = false;
+  if(title.value === "" || author.value === "" || pages.value === ""){
+    event.preventDefault();
+  } else if (title.value != "" && author.value != "" && pages.value != ""){
+    flag = true;
+  }
+  if(flag){
+    myLibary.push([{
+        title: title.value,
+        author: author.value,
+        pages: pages.value,
+        status: status.checked
+    }]);
+    showForm();
+    addBookToLibary();
+  }
+})
+
+
+
+/*(function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
 }
 
-Book.prototype.deleteMyLibary = function() {
-        delete this.myLibary;
-}
+Book.prototype.updateMyLibary = function () {
+  myLibary.push([
+    {
+      title: this.title,
+      author: this.author,
+      pages: this.pages,
+      status: this.status,
+    },
+  ]);
+};
 
+Book.prototype.deleteMyLibary = function () {
+  delete this.myLibary;
+};
 
+let bookOne = new Book("Atomic Habit", "James Clear", 400, true);
 
-let bookOne = new Book("Atomic Habit",
-    "James Clear",
-    400,
-    "completed"
+let bookTwo = new Book(
+  "12 Rules for Life",
+  "Jorden Peterson",
+  300,
+  false
 );
 
-let bookTwo = new Book ("12 Rules for Life", 
-    "Jorden Peterson", 
-    300, 
-    "not read yet"
-);
+let bookThree = new Book(
+  "Harry Porter",
+  "J. K. Rolling",
+  "3407",
+  false
+)
 
 bookOne.updateMyLibary();
 
 bookTwo.updateMyLibary();
 
-addBookToLibary();
+bookThree.updateMyLibary();)*/
 
 function addBookToLibary() {
-    myLibary.forEach((book) => {
-        book.forEach((detials) => {
-            console.log(detials);
-            bookContainer.innerText += "Title : "+ detials.title + "\n" 
-                + "Author : " + detials.author + "\n" 
-                + "Page : " + detials.pages + "\n" 
-                + "Status : " + detials.status  + "\n";
-        })
-    })
+  myLibary.forEach((book) => {
+    book.forEach((detials) => {
+        let newBook = document.createElement("div");
+        let deleteBook = document.createElement("div");
+        deleteBook.className = "close";
+        newBook.classList = "box";
+        newBook.innerText = "Title : " +
+        detials.title +
+        "\n" +
+        "Author : " +
+        detials.author +
+        "\n" +
+        "Pages : " +
+        detials.pages +
+        readStatus(detials.status);
+        bookContainer.append(newBook);
+        newBook.appendChild(deleteBook);
+    });
+  });
 }
 
-newBookButton.addEventListener('click', ()=>{
-    showForm();
-})
-
-function showForm() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function readStatus(status) {
+  if(status){
+    return "\nStatus : Completed";
+  } else {
+    return "\nStatus : Incomplete";
   }
+}
