98dcb06ae06165334f44a818fd5002d7261e4b6b
roman <51534566+roman-thapa@users.noreply.github.com>
2023-04-20 12:53:16 +0530

    Add delete button that delete book in libary

--- script.js
+++ script.js
@@ -40,7 +40,6 @@ addBook.addEventListener('click', (event) => {
         status: status.checked
     }]);
     showForm();
-    //addBookToLibary();
     showBook(indexOfBook);
     indexOfBook++;
   }
@@ -49,8 +48,11 @@ addBook.addEventListener('click', (event) => {
 function showBook(indexOfBook) {
   myLibary[indexOfBook].forEach((detials) => {
     let newBook = document.createElement("div");
-    let deleteBook = document.createElement("div");
-    deleteBook.className = "close";
+    let removeBookButton = document.createElement("button");
+    removeBookButton.className = "close";
+    newBook.id = indexOfBook;
+    removeBookButton.innerText = "X";
+    removeBookButton.onclick = function() { deleteBook(indexOfBook); };
     newBook.classList = "box";
     newBook.innerText = "Title : " +
     detials.title +
@@ -62,7 +64,7 @@ function showBook(indexOfBook) {
     detials.pages +
     readStatus(detials.status);
     bookContainer.append(newBook);
-    newBook.appendChild(deleteBook);
+    newBook.appendChild(removeBookButton);
 });
 }
 
@@ -74,66 +76,8 @@ function readStatus(status) {
   }
 }
 
-/*(function Book(title, author, pages, status) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.status = status;
+function deleteBook(id) {
+  delete myLibary[id];
+  let bookToDelete = document.getElementById(id);
+  bookToDelete.remove();
 }
-
-Book.prototype.updateMyLibary = function () {
-  myLibary.push([
-    {
-      title: this.title,
-      author: this.author,
-      pages: this.pages,
-      status: this.status,
-    },
-  ]);
-};
-
-Book.prototype.deleteMyLibary = function () {
-  delete this.myLibary;
-};
-
-let bookOne = new Book("Atomic Habit", "James Clear", 400, true);
-
-let bookTwo = new Book(
-  "12 Rules for Life",
-  "Jorden Peterson",
-  300,
-  false
-);
-
-let bookThree = new Book(
-  "Harry Porter",
-  "J. K. Rolling",
-  "3407",
-  false
-)
-
-bookOne.updateMyLibary();
-
-bookTwo.updateMyLibary();
-
-bookThree.updateMyLibary();)
-
-function addBookToLibary() {
-  myLibary.forEach((book) => {
-    book.forEach((detials) => {
-        let newBook = document.createElement("div");
-        newBook.classList = "box";
-        newBook.innerText = "Title : " +
-        detials.title +
-        "\n" +
-        "Author : " +
-        detials.author +
-        "\n" +
-        "Pages : " +
-        detials.pages +
-        readStatus(detials.status);
-        bookContainer.append(newBook);
-        newBook.appendChild(deleteBook);
-    });
-  });
-}*/
